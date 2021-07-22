56c103d838b67490d05985e1a83aef519087f73a
Kecalkoatl <drfreezbt@gmail.com>
2021-07-22 18:38:27 +0300

    Commit 1

--- src/ReadCalc.java
+++ src/ReadCalc.java
@@ -15,17 +15,22 @@ public class ReadCalc {
         String text = scanner.nextLine();
         String[] blocks = text.split(" ");
         if (blocks.length < 0 || blocks.length > 3 ) {
-            throw new IllegalArgumentException("Выражение введено неверно!");
+            throw new IllegalArgumentException("Калькулятор может работать только с двумя числами!");
         }
-        operation = blocks[1];
-        this.operation1 = operation.charAt(0);
-        this. flag = false;
-        for (int i = 0; i < roman.length; i++){
-            if (roman[i].equals(blocks[0]) || roman[i].equals(blocks[2])) {
-                flag = true;
+        try {
+            operation = blocks[1];
+            this.operation1 = operation.charAt(0);
+            this.flag = false;
+            for (int i = 0; i < roman.length; i++) {
+                if (roman[i].equals(blocks[0]) || roman[i].equals(blocks[2])) {
+                    flag = true;
+                }
             }
+            //Да, тут циклу достаточно оператора or. И это работает благодаря конвертеру.
         }
-        //Да, тут циклу достаточно оператора or. И это работает благодаря конвертеру.
+        catch (ArrayIndexOutOfBoundsException e){
+            throw new ArrayIndexOutOfBoundsException("Выражение введено неверно!");
+        }
         try {
             if (flag) {
                 num1 = RomanToArabic.RomanToArabic(blocks[0]);
